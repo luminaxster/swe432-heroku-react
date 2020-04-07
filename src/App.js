2b5c6e32155168a0d89e1258cc4998b17f011065
David Samudio <luminaxster@gmail.com>
2020-04-07 17:07:40 -0400

    fixing errors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Fetcher from './Fetcher';
+import Hooks from './Hooks';
 
 function App() {
   return (
     <div className="App">
-      <Fetcher url={'https://randomuser.me/api/'}/>
+      <Hooks/>
     </div>
   );
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Fetcher url={'https://randomuser.me/api/'}/>
+//     </div>
+//   );
+// }
 
 export default App;
--- src/Hooks.js
+++ src/Hooks.js
@@ -1,18 +1,9 @@
 import React, { useState, useEffect } from 'react';
 
-export default function Fetcher(props) {
+
+export default function Hooks(props) {
     const { classes, url } = props;
     let [clicks, setClicks] = useState(0);
-    const [response, setResponse] = useState(null);
-
-    useEffect( ()=> {
-      const  fetchData= async()=>{
-        const res = await fetch(url);
-        const json = await res.json();
-        setResponse(json);
-      }
-      fetchData();
-    }, []);
 
     const doSomething = function (event) {
         console.log(event.currentTarget.getAttribute('data-something'));
@@ -23,7 +14,6 @@ export default function Fetcher(props) {
             <p>click count:{clicks}</p>
             <button onClick={doSomething} data-something="Default">
                 Default</button>
-            <p>{response && JSON.stringify(response) || 'waiting' }</p>
         </React.Fragment>
 
     );
