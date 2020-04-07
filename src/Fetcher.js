a996907e2fb47f28f0c53bb85aea3f8687b432ec
David Samudio <luminaxster@gmail.com>
2020-04-07 16:14:17 -0400

    added Fetch example

---
+++ src/Fetcher.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+
+
+export default function Fetcher(props) {
+    const { classes, url } = props;
+    let [clicks, setClicks] = useState(0);
+    const [response, setResponse] = useState(null);
+
+    useEffect(async function() {
+        const res = await fetch(url, options);
+        const json = await res.json();
+        setResponse(json);
+    });
+
+    const doSomething = function (event) {
+        console.log(event.currentTarget.getAttribute('data-something'));
+        setClicks(clicks + 1);
+    }
+    return (
+        <React.Fragment>
+            <p>click count:{clicks}</p>
+            <button onClick={doSomething} data-something="Default">
+                Default</button>
+            <p>{response ? 'waiting' : response}</p>
+        </React.Fragment>
+
+    );
+}
