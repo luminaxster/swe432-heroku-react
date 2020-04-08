881270fa46363d458aa4ade2ed90c42c1bf9c02d
David Gonzalez <luminaxster@gmail.com>
2020-04-07 21:20:10 -0400

    Update App.js
    both examples at once

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import './App.css';
 import Fetcher from './Fetcher';
-// import Hooks, {aFunc} from './Hooks';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Hooks name={'David'}/>
-//     </div>
-//   );
-// }
+import Hooks, {aFunc} from './Hooks';
 
 function App() {
   return (
     <div className="App">
+      <Hooks name={aFunc().name}/>
       <Fetcher url={'https://randomuser.me/api/'}/>
     </div>
   );
