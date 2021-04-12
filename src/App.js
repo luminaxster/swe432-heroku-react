32bf55f5fcaaf62212c0b2e9c4a1ec0ce7448242
David Gonzalez <luminaxster@gmail.com>
2021-04-12 18:32:30 -0400

    Adds GitHub Link and custom hook example

--- src/App.js
+++ src/App.js
@@ -6,11 +6,41 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import IconButton from '@material-ui/core/IconButton';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 import Fetcher from './components/Fetcher';
 import Hooks, {aFunc} from './components/Hooks';
 import ToggleButtons from './components/ToggleButtons';
 import PopcornSales from './components/PopcornSales';
+
+export const useLinkOpener = () => {
+  const windowObjectReferences = useMemo(() => ({}), []);
+  const openRequestedSinglePopup = useCallback(
+    (url, target, windowFeatures = "resizable,scrollbars,status") => {
+      if (window !== window.top) {
+        window.open(
+          url,
+          target,
+          windowFeatures ? `${windowFeatures},noreferrer` : "noreferrer"
+        );
+        return true;
+      }
+
+      const windowObjectReference = windowObjectReferences[url];
+      if (!windowObjectReference || windowObjectReference.closed) {
+        windowObjectReferences[url] = window.open(url, target, windowFeatures);
+        return true;
+      }
+
+      windowObjectReference.focus();
+      return false;
+    },
+    [windowObjectReferences]
+  );
+
+  return [windowObjectReferences, openRequestedSinglePopup];
+};
 
 export default function App(props) {
   const [currentTab, setCurrentTab] = useState(0);
@@ -28,14 +58,30 @@ export default function App(props) {
     }),
     []
   );
+  
+  const [, openLink] = useLinkOpener();
+  const handleChangeOpenGitHubLink = useCallback(() => {
+    openLink(
+      "https://github.com/luminaxster/swe432-heroku-react",
+      "https://github.com/luminaxster"
+    );
+  }, [openLink]);
 
   return (
     <Box sx={rootSX}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" component="div">
+          <Typography variant="h6" component="div" noWrap >
               SWE 432 React examples
           </Typography>
+          <Box sx={{ flexGrow: 1 }} />
+          <IconButton
+              edge="end"
+              color="inherit"
+              onClick={handleChangeOpenGitHubLink}
+          >
+            <GitHubIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
       <Box sx={tabsSX}>
